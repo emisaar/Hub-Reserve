50a7c40d2baeaf1ef388c00f4e97bdc28be730ff
emisaar <emisaar0406@gmail.com>
2022-09-19 15:43:36 -0500

    Initial Xcode commit

---
+++ Hub-Reserve/RecoverPwdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecoverPwdViewController.swift
+//  Hub-Reserve
+//
+//  Created by Emi Saucedo on 18/09/22.
+//
+
+import UIKit
+
+class RecoverPwdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
