dc23c223931c1e1aecf0b16ef9512269d781f9a2
AlejandroDiaz10 <75130652+AlejandroDiaz10@users.noreply.github.com>
2022-10-09 18:10:38 -0500

    Más datos para cambiar la contraseña

--- Hub-Reserve/Controllers/Profile/EditPasswordViewController.swift
+++ Hub-Reserve/Controllers/Profile/EditPasswordViewController.swift
@@ -39,6 +39,21 @@ class EditPasswordViewController: UIViewController {
             print("WRONG MAIL")
             return
         }
+        
+        /* 
+        if passwordTextField.text != "ala"{
+            showAlertErrorChangePwd()
+        }
+         */
+        
+        if newPasswordTextField.text != newPasswordConfirmTextField.text {
+            showIncorrectNewPassword()
+        }
+        
+        if !checkPassword(password: newPasswordTextField.text ?? ""){
+            showAlertInsecurePassword()
+        }
+        
         Task{
             do{
                 let response = try await WebService().changePassword(email: userEmail, password: passwordTextField.text!)
@@ -54,6 +69,8 @@ class EditPasswordViewController: UIViewController {
             }
         }
     }
+    
+    
     @IBAction func textEditingChanged(_ sender: UITextField) {
         updateSaveButtonState()
     }
@@ -68,6 +85,41 @@ class EditPasswordViewController: UIViewController {
 
         update.isEnabled = !password.isEmpty && !newPassword.isEmpty && !newPasswordConfirm.isEmpty
     }
+    
+    func checkPassword(password: String) -> Bool {
+        //ASCII values: a(97) - z(122)
+        //ASCII values: A(65) - Z(90)
+        //ASCII values: 0(48) - 9(57)
+        
+        var flagCapLetter = false
+        var flagLowLetter = false
+        var flagNum = false
+        var flagSymbol = false
+        
+        if password.count < 8{
+            return false
+        }
+        for p in password{
+            let ascii = p.asciiValue
+            if ascii ?? 0 >= 65, ascii ?? 0 <= 90{
+                flagCapLetter = true
+            }
+            else if ascii ?? 0 >= 48, ascii ?? 0 <= 57{
+                flagNum = true
+            }
+            else if ascii ?? 0 >= 97, ascii ?? 0 <= 122{
+                flagLowLetter = true
+            }
+            else{
+                flagSymbol = true
+            }
+        }
+        if flagNum, flagSymbol, flagLowLetter, flagCapLetter{
+            return true
+        }
+        return false
+    }
+    
 
     func showAlertChangePasswordDone(){
         // Create Alert View
@@ -79,8 +131,22 @@ class EditPasswordViewController: UIViewController {
     func showAlertErrorChangePwd(){
         // Create Alert View
         let alertView = UIAlertController(title: "Hubo un error", message: "La contraseña actual no es la correcta.", preferredStyle: .alert)
-        alertView.addAction(UIAlertAction(title:"Aceptar", style: .default, handler: {(_) in self.changeScreen()}))
+        alertView.addAction(UIAlertAction(title:"Aceptar", style: .cancel, handler: nil))
         self.present(alertView, animated: true, completion: nil)
+    }
+    
+    func showIncorrectNewPassword(){
+        // Create Alert View
+        let alertView = UIAlertController(title: "Hubo un error", message: "La nueva contraseña no coincide con la verifiacicón.", preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title:"Aceptar", style: .cancel, handler: nil))
+        self.present(alertView, animated: true, completion: nil)
+    }
+    
+    func showAlertInsecurePassword(){
+        // Create Alert View
+        let alertView = UIAlertController(title: "Atención", message: "Su nueva contraseña es insegura.\nSe recomienda: \nUsar al menos 1 letra mayúscula\nUsar al menos 1 número \nUsar al menos 1 símbolo\nContraseña con longitud mínimo de 8 caracteres", preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title:"Aceptar", style: .default, handler: nil))
+        self.present(alertView, animated: false, completion: nil)
     }
     
     func displayError(_ error: Error, title: String) {
