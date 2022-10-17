b30af755a4598c2ee4a99136263e98658acb3b3c
emisaar <emisaar0406@gmail.com>
2022-10-17 00:53:22 -0500

    Recuperación del repo para salas y hardware

--- Hub-Reserve/Controllers/Rooms/RoomsTableViewController.swift
+++ Hub-Reserve/Controllers/Rooms/RoomsTableViewController.swift
@@ -1,68 +1,48 @@
 //
-//  RoomsTableViewController.swift
+//  RoomTableViewCell.swift
 //  Hub-Reserve
 //
-//  Created by Emi Saucedo on 22/09/22.
+//  Created by Emi Saucedo on 25/09/22.
 //
 
 import UIKit
 
-class RoomsTableViewController: UITableViewController {
-    /*
-     Resource type id : int
-     
-     var rooms =
-     */
-    //var rooms = Room.roomList()
-    let defaults = UserDefaults.standard
-//    var roomsControlador = WebService().resources(jwt: "ERGERGJANEFW")
-//    { result in
-//        switch result{
-//        case .success(let resourceName):
-//           print(resourceName)
-//        case .failure(let error):
-//            print(error)
-//        }
-//    }
+class RoomTableViewCell: UITableViewCell {
     
-    var recursos = Resources()
-    var cellLabel = ""
-    var cellID = 0
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var resourceLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var starIcon: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let defaults = UserDefaults.standard
-        guard let token = defaults.string(forKey: "jwt") else {
-            return
-        }
-        
-        print("TOKEN")
-        print(token)
-        
-        Task{
-            do{
-                let recursos = try await WebService().getResources(token: token)
-                
-                var rooms = [Resource]()
-                
-                for r in recursos {
-                    if r.category == "Espacio físico" {
-                        rooms.append(r)
-                    }
-                }
-                
-                updateUI(with: rooms)
-            }catch{
-                displayError(NetworkError.noData, title: "No se pudo acceder a los recursos")
-            }
-        }
+    var id = 0
+    
+    func update(r: Resource) {
+        icon.image = UIImage(systemName: "signpost.right")
+        resourceLabel.text = r.name
+        statusLabel.text = r.description
+        id = r.id
     }
     
-    func updateUI(with recursos: Resources){
-        DispatchQueue.main.async {
-            
-            self.recursos = recursos
-            self.tableView.reloadData()
+    func changeIcon() {
+        starIcon.setImage(UIImage(systemName: "star.fill"), for: .normal)
+    }
+    
+    @IBAction func addFav(_ sender: Any) {
+        Task{
+            let defaults = UserDefaults.standard
+            guard let token = defaults.string(forKey: "jwt") else {
+                return
+            }
+
+            print("TOKEN")
+            print(token)
+            print("RESOURCE ID \(id)")
+            do {
+                let recursos = try await WebService().addFavorites(token: token, resource: id)
+                changeIcon()
+            } catch {
+                displayError(NetworkError.noData, title: "No se pudo añadir a favoritos")
+            }
         }
     }
     
@@ -70,177 +50,19 @@ class RoomsTableViewController: UITableViewController {
             DispatchQueue.main.async {
                 let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
             }
         }
-    // MARK: - Table view data source
-
-    /*PASO 0 crear método unwindToEmojiTableView para cerrar la vista de la tabla estática
-     Luego de la definición se deben crear los segues de tipo unwind, es decir, segues hacia el proxy EXIT*/
     
-    /* UNCOMMENT FOR EDITABLE TABLES
-    @IBAction func unwindToEmojiTableView(segue: UIStoryboardSegue) {
-        guard segue.identifier == "saveUnwind",
-            let sourceViewController = segue.source as? EditReservationsTableViewController,
-              let reserva = sourceViewController.reserva else { return }
-
-        if let selectedIndexPath = tableView.indexPathForSelectedRow {
-            reservations[selectedIndexPath.row] = reserva
-            tableView.reloadRows(at: [selectedIndexPath], with: .none)
-        } else {
-            let newIndexPath = IndexPath(row: reservations.count, section: 0)
-            reservations.append(reserva)
-            tableView.insertRows(at: [newIndexPath], with: .automatic)
-        }
-    }
     
-    /*
-     leer esta nota
-     With prepare(for:sender:), UIKit creates the view controller and passes the instance to the starting point view controller’s prepare(for:sender:) for configuration. With an IBSegueAction, you create the view controller fully configured and pass it to UIKit for display
-     */
-    
-    // CONNECT DIRECTLY, DO NOT COPY IT
-    @IBSegueAction func editReserva(_ coder: NSCoder, sender: Any?, segueIdentifier: String?) -> EditReservationsTableViewController? {
-        if let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell) {
-            // Editing Emoji
-            let reservaToEdit = reservations[indexPath.row]
-            return EditReservationsTableViewController(coder: coder, r: reservaToEdit)
-        } else {
-            // Adding Emoji
-            return EditReservationsTableViewController(coder: coder, r: nil)
-        }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
 
-    @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
-        let tableViewEditingMode = tableView.isEditing
-            tableView.setEditing(!tableViewEditingMode, animated: true)
-    }
-    
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-        let movedReservation = reservations.remove(at: fromIndexPath.row)
-        reservations.insert(movedReservation, at: to.row)
-    }
-    */
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        //return rooms.count
-        return recursos.count
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RoomTableViewCell
-
-        // Configure the cell...
-        let index = indexPath.row
-        //let resource = rooms[index]
-        let resource = recursos[index]
-        cell.update(r: resource)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //cellLabel = rooms[indexPath.row].name
-        cellLabel = recursos[indexPath.row].name
-        cellID = recursos[indexPath.row].id
-
-//        performSegue(withIdentifier: "Reservar", sender: nil)
-        
-        let vc = storyboard?.instantiateViewController(withIdentifier: "Reservar") as? ReserveViewController
-        vc?.resourceText = cellLabel
-        vc?.idResourceText = cellID
-        navigationController?.pushViewController(vc!, animated: true)
-    }
-    
-    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        return .delete
-    }
-
-    
-    /* UNCOMMENT FOR EDITABLE TABLES
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let alertView = UIAlertController(title: "Advertencia", message: "¿Seguro que desea eliminar la reservación? La acción no se puede deshacer", preferredStyle: .alert)
-            alertView.addAction(UIAlertAction(title:"Cancelar", style: .cancel, handler: nil))
-            alertView.addAction(UIAlertAction(title:"Aceptar", style: .default, handler: {_ in
-                // Delete the row from the data source
-                self.reservations.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }))
-            self.present(alertView, animated: true, completion: nil)
-
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-  */
-    
-//    // Override to support editing the table view.
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            // Delete the row from the data source
-//            reservations.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//        } else if editingStyle == .insert {
-//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-//        }
-//    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        if segue.identifier == "Reservar"{
-            // Pass the selected object to the new view controller.
-            let nextScreen = segue.destination as! ReserveViewController
-
-            nextScreen.resourceText = cellLabel
-        }
-    }
-    */
 }
