c699b2dfc47cae00b0518cdafcac9ba95c7adae0
emisaar <emisaar0406@gmail.com>
2022-10-17 00:57:39 -0500

    Recuperación de rooms cell

--- Hub-Reserve/Controllers/Rooms/RoomTableViewCell.swift
+++ Hub-Reserve/Controllers/Rooms/RoomTableViewCell.swift
@@ -12,20 +12,47 @@ class RoomTableViewCell: UITableViewCell {
     @IBOutlet weak var icon: UIImageView!
     @IBOutlet weak var resourceLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var starIcon: UIButton!
+    
+    var id = 0
     
     func update(r: Resource) {
         icon.image = UIImage(systemName: "signpost.right")
         resourceLabel.text = r.name
-        statusLabel.text = statusCheck(status: r.active)
+        statusLabel.text = r.description
+        id = r.id
     }
     
-    func statusCheck(status: Bool) -> String {
-        if (status == true){
-            return "Disponible"
-        } else {
-            return "No Disponible"
+    func changeIcon() {
+        starIcon.setImage(UIImage(systemName: "star.fill"), for: .normal)
+    }
+    
+    @IBAction func addFav(_ sender: Any) {
+        Task{
+            let defaults = UserDefaults.standard
+            guard let token = defaults.string(forKey: "jwt") else {
+                return
+            }
+
+            print("TOKEN")
+            print(token)
+            print("RESOURCE ID \(id)")
+            do {
+                let recursos = try await WebService().addFavorites(token: token, resource: id)
+                changeIcon()
+            } catch {
+                displayError(NetworkError.noData, title: "No se pudo añadir a favoritos")
+            }
         }
     }
+    
+    func displayError(_ error: Error, title: String) {
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            }
+        }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
