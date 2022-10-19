021d99c1f5c241117e2f5745db23061f0f7e870a
AlejandroDiaz10 <75130652+AlejandroDiaz10@users.noreply.github.com>
2022-10-18 19:56:45 -0500

    No se compara reservations y day

--- Hub-Reserve/Controllers/Calendar/CalendarViewController.swift
+++ Hub-Reserve/Controllers/Calendar/CalendarViewController.swift
@@ -7,14 +7,109 @@
 
 import UIKit
 
-class CalendarViewController: UIViewController, UICalendarSelectionSingleDateDelegate {
+fileprivate var aView : UIView?
 
+class CalendarViewController: UIViewController, UICalendarSelectionSingleDateDelegate {
+    
+    var reservations = Reservas()
+    
+    func showSpinner(){
+        aView = UIView(frame: self.view.bounds)
+        aView?.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView(style: .whiteLarge)
+        ai.center = aView!.center
+        ai.startAnimating()
+        aView?.addSubview(ai)
+        self.view.addSubview(aView!)
+    }
+    
+    func removeSpinner(){
+        aView?.removeFromSuperview()
+        aView = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let defaults = UserDefaults.standard
+        guard let token = defaults.string(forKey: "jwt") else {
+            return
+        }
+        
+        print("TOKEN")
+        print(token)
+        Task{
+            do{
+                showSpinner()
+                let reservas = try await WebService().getReservas(token: token)
+                
+                var newReservas = [Reserva]()
+                
+                for r in reservas {
+                    if r.status != "Cancelada" && r.status != "Terminada" && r.status != "Cambiada" {
+                        newReservas.append(r)
+                    }
+                }
+                
+//                print(newReservas)
+//                updateUI(with: newReservas)
+                reservations = newReservas
+                removeSpinner()
+            }catch{
+                displayError(NetworkError.noData, title: "No se pudo acceder a las reservas")
+                removeSpinner()
+            }
+        }
         createCalendar()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        let defaults = UserDefaults.standard
+        guard let token = defaults.string(forKey: "jwt") else {
+            return
+        }
+        
+        print("TOKEN")
+        print(token)
+        Task{
+            do{
+                let reservas = try await WebService().getReservas(token: token)
+                
+                var newReservas = [Reserva]()
+                
+                for r in reservas {
+                    if r.status != "Cancelada" && r.status != "Terminada" && r.status != "Cambiada" {
+                        newReservas.append(r)
+                    }
+                }
+                
+//                print(newReservas)
+//                updateUI(with: newReservas)
+                reservations = newReservas
+                removeSpinner()
+            }catch{
+                displayError(NetworkError.noData, title: "No se pudo acceder a las reservas")
+                removeSpinner()
+            }
+        }
+    }
+/*
+    func updateUI(with reservas: Reservas){
+        DispatchQueue.main.async {
+            self.reservations = reservas
+//            self.tableView.reloadData()
+        }
+    }
+ */
+    
+    func displayError(_ error: Error, title: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
     
     func createCalendar() {
@@ -46,15 +141,6 @@ class CalendarViewController: UIViewController, UICalendarSelectionSingleDateDel
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     func changeDateFormat(dateString: String) -> String {
         let isoDate1 = dateString
@@ -82,11 +168,15 @@ extension CalendarViewController: UICalendarViewDelegate {
         guard let day = dateComponents.day else {
             return nil
         }
+        
+        print("Reservations-----")
+        print(reservations)
 
         if !day.isMultiple(of: 2) {
             return UICalendarView.Decoration.default(color: .systemGreen, size: .small)
         }
 
+        
         return nil
     }
     
